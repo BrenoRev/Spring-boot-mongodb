12e38b75136c650d704c0e75281481150254eb8f
Breno Rev <silvabreno462@gmail.com>
2021-06-15 00:16:07 -0300

    Search an user by id

--- src/main/java/com/brenodev/workshopmongo/services/UserService.java
+++ src/main/java/com/brenodev/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.brenodev.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.brenodev.workshopmongo.domain.User;
 import com.brenodev.workshopmongo.repository.UserRepository;
+import com.brenodev.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,4 +19,11 @@ public class UserService {
 	public List<User> findAll(){
 		return repo.findAll();
 	}
-}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		// RETORNA UM OBJETO OU UMA EXCEÇÃO
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+
+	}
+	}
